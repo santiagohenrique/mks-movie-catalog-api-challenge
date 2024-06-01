d0f75af08c53f9e9a7985e6bc31bcc3fb37f9bba
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-06-01 18:34:04 -0300

    Genre CRUD completed

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { GenreService } from './genre.service';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { GenreDto } from './dto/genre.dto';
 import { Genre } from './entity/genre.entity';
-import { CreateGenreDto } from './dto/create-genre.dto';
+import { GenreService } from './genre.service';
 
 @Controller('genre')
 export class GenreController {
@@ -13,9 +13,25 @@ export class GenreController {
         return this.genreService.findAll();
     }
 
+    @Get("/:id")
+    async findById(@Param('id') id): Promise<Genre>{
+        return await this.genreService.findById(id);
+    }
+
     @Post()
-    async create(@Body() createGenreDto: CreateGenreDto): Promise<Genre> {
-        return await this.genreService.create(createGenreDto);
+    async create(@Body() createGenreRequest: GenreDto): Promise<Genre> {
+        return await this.genreService.create(createGenreRequest);
+    }
+
+    @Put("/:id")
+    async update(@Param('id') id:string, @Body() genreUpdateRequest: GenreDto): Promise<GenreDto>{
+        return await this.genreService.update(id, genreUpdateRequest);
+    }
+
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete("/:id")
+    async delete(@Param('id') id): Promise<void>{
+        return await this.genreService.delete(id);
     }
 
 
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { GenreDto } from './dto/genre.dto';
 import { Genre } from './entity/genre.entity';
-import { CreateGenreDto } from './dto/create-genre.dto';
 
 @Injectable()
 export class GenreService {
@@ -13,12 +13,31 @@ export class GenreService {
     ) {}
 
     async findAll(): Promise<Genre[]> {
-        return this.genreRepository.find();
+        return await this.genreRepository.find();
     }
 
-    async create(createGenreDto: CreateGenreDto): Promise<Genre> {
-        const genre = await this.genreRepository.create(createGenreDto);
+    async findById(id: string): Promise<Genre>{
+        const genreFound = await this.genreRepository.findOne({ where: {id : id}});
+        if(!genreFound){
+            throw new NotFoundException(`Genre with id ${id} not found!`)
+        }
+        return genreFound;
+    }
+
+    async create(createGenreRequest: GenreDto): Promise<Genre> {
+        const genre = await this.genreRepository.create(createGenreRequest);
         return this.genreRepository.save(genre);
+    }
+
+    async delete(id: string): Promise<void>{
+        const genreFound = await this.genreRepository.findOne({ where: {id : id}});
+
+        if(!genreFound){
+            throw new NotFoundException(`Genre with id ${id} not found!`)
+        }
+
+        await this.genreRepository.remove(genreFound);
+
     }
 
 }
