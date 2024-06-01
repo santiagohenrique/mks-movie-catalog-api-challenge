88eb83c80cbeda3ae9f7df33e73729b6983e1f3b
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-06-01 18:45:02 -0300

    Fixing constraint error when deleting genre

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { GenreDto } from './dto/genre.dto';
 import { Genre } from './entity/genre.entity';
 
@@ -24,6 +24,22 @@ export class GenreService {
         return genreFound;
     }
 
+    async update(id: string, genreDtoRequest: GenreDto): Promise<GenreDto>{
+        const genreToUpdate = await this.genreRepository.findOne({ where: {id : id}});
+        if(!genreToUpdate){
+            throw new NotFoundException(`Genre with id ${id} not found!`)
+        }
+        genreToUpdate.name = genreDtoRequest.name;
+
+        await this.genreRepository.save(genreToUpdate);
+
+        const updatedGenreDto: GenreDto = {
+            name: genreToUpdate.name
+        }
+
+        return updatedGenreDto;
+    }
+
     async create(createGenreRequest: GenreDto): Promise<Genre> {
         const genre = await this.genreRepository.create(createGenreRequest);
         return this.genreRepository.save(genre);
@@ -36,7 +52,16 @@ export class GenreService {
             throw new NotFoundException(`Genre with id ${id} not found!`)
         }
 
-        await this.genreRepository.remove(genreFound);
+        try {
+            await this.genreRepository.remove(genreFound);
+        } catch (error) {
+            if (error instanceof QueryFailedError) {
+                throw new BadRequestException(
+                    'Cannot delete genre as it is associated with one or more movies.'
+                );
+            }
+            throw error;
+        }
 
     }
 
